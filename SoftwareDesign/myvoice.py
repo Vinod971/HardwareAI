1c2fc95f2e6a0335ff3e85223975a363c7fe3899
Vinod971 <157921299+Vinod971@users.noreply.github.com>
2025-06-05 11:59:25 -0700

    Delete SoftwareDesign directory

--- SoftwareDesign/myvoice.py
+++
@@ -1,8 +0,0 @@
-def save_sample_voice(filename='my_voice_sample.wav'):
-    # This is where you can record your voice and save it as a sample.
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Please say something for voice sample...")
-        audio = recognizer.listen(source)
-        with open(filename, 'wb') as f:
-            f.write(audio.get_wav_data())
